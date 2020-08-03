1fa294d0b93b9fd7f819860eaeadc8af12d93bfd
Matthew Cavallo <matt.cavallo@outlook.com>
2020-08-03 13:59:00 -0500

    First Commit3

--- FTPFA5601/Function1.cs
+++ FTPFA5601/Function1.cs
@@ -50,17 +50,5 @@ namespace FTPFA5601
 
             return new OkObjectResult(responseMessage);
         }
-
-        public static byte[] FileToByteArray(string fileName)
-        {
-            byte[] buff = null;
-            FileStream fs = new FileStream(fileName,
-                                           FileMode.Open,
-                                           FileAccess.Read);
-            BinaryReader br = new BinaryReader(fs);
-            long numBytes = new FileInfo(fileName).Length;
-            buff = br.ReadBytes((int)numBytes);
-            return buff;
-        }
     }
 }
